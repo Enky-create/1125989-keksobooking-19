eca74851d296f31bbec242fc0b296d740b386c3c
Enky-create <komkowvadim@gmail.com>
2020-03-12 23:55:19 +0700

    Для Тревиса

--- js/data.js
+++ js/data.js
@@ -1,25 +1,6 @@
 'use strict';
 (function () {
   var arrayPin;
-  var getRandomInRange = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  var getRandomInArray = function (array) {
-    var min = 0;
-    var max = array.length - 1;
-    var index = getRandomInRange(min, max);
-    return array[index];
-  };
-
-  var getRandomArray = function (array) {
-    var length = getRandomInRange(1, array.length);
-    var newArray = [];
-    for (var i = 0; i < length; i++) {
-      newArray[i] = array[i];
-    }
-    return newArray;
-  };
   var errorFill = function (message) {
     var div = document.createElement('div');
     var mapPins = document.querySelector('.map__pins');
