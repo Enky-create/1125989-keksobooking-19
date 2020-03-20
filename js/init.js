6d49441fe8b75256fde1e7a10f0e53cc80861ebf
Enky-create <komkowvadim@gmail.com>
2020-03-20 19:58:30 +0700

    Блокирует форму после отправки

--- js/init.js
+++ js/init.js
@@ -1,6 +1,9 @@
 'use strict';
 (function () {
   var init = function () {
+    var map = document.querySelector('.map');
+    var pins = document.querySelector('.map__pins');
+    var nested = document.querySelectorAll('.map__pin:not(.map__pin--main)');
     var form = document.querySelector('.ad-form');
     var roomNumber = document.querySelector('#room_number');
     var capacity = document.querySelector('#capacity');
@@ -12,6 +15,8 @@
     var yString = mapPinMain.style.top.replace('px', '');
     var x = parseInt(xString, 10) + window.constant.MUFFIN_RADIUS;
     var y = parseInt(yString, 10) + window.constant.MUFFIN_RADIUS;
+    map.className = 'map';
+    map.classList.add('map--faded');
     for (var i = 0; i < fieldsets.length; i++) {
       fieldsets[i].setAttribute('disabled', 'disabled');
     }
@@ -20,7 +25,13 @@
     if (+roomNumber.value < +capacity.value) {
       roomNumber.setCustomValidity('Количество комнат не соответствует количеству гостей');
     }
+    for (var a = 0; a < nested.length; a++) {
+      pins.removeChild(nested[a]);
+    }
+    form.className = 'ad-form';
+    form.classList.add('ad-form--disabled');
     form.reset();
+    window.map.activateSite();
   };
   window.init = {
     doInit: init
--- js/map.js
+++ js/map.js
@@ -1,76 +1,82 @@
 'use strict';
 (function () {
-  var filters = document.querySelector('.map__filters');
-  var fieldsets = document.querySelectorAll('fieldset');
-  var mapPinMain = document.querySelector('.map__pin--main');
-  var map = document.querySelector('.map');
-  var pins = document.querySelector('.map__pins');
-  var activate = function () {
-    var nested = document.querySelectorAll('.map__pin:not(.map__pin--main)');
-    var form = document.querySelector('.ad-form');
+  var activateSite = function () {
+    var filters = document.querySelector('.map__filters');
+    var fieldsets = document.querySelectorAll('fieldset');
+    var mapPinMain = document.querySelector('.map__pin--main');
+    var map = document.querySelector('.map');
+    var pins = document.querySelector('.map__pins');
+    var activate = function () {
+      debugger;
+      //var nested = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+      var form = document.querySelector('.ad-form');
 
-    map.classList.remove('map--faded');
-    for (var i = 0; i < nested.length; i++) {
-      pins.removeChild(nested[i]);
-    }
+      map.classList.remove('map--faded');
+      /*for (var i = 0; i < nested.length; i++) {
+        pins.removeChild(nested[i]);
+      }*/
 
-    window.data.fillData();
-    for (var b = 0; b < fieldsets.length; b++) {
-      fieldsets[b].removeAttribute('disabled');
-    }
-    form.classList.remove('ad-form--disabled');
-    filters.classList.remove('ad-form--disabled');
-  };
+      window.data.fillData();
+      for (var b = 0; b < fieldsets.length; b++) {
+        fieldsets[b].removeAttribute('disabled');
+      }
+      form.classList.remove('ad-form--disabled');
+      filters.classList.remove('ad-form--disabled');
+    };
 
-  var pinMousedownHandler = function (evt) {
-    if (evt.button === 0) {
+    var pinMousedownHandler = function (evt) {
+      if (evt.button === 0) {
+        activate();
+        mapPinMain.removeEventListener('click', pinClickHandler);
+        mapPinMain.removeEventListener('mousedown', pinMousedownHandler);
+      }
+    };
+
+    var pinClickHandler = function () {
       activate();
-      mapPinMain.removeEventListener('click', pinClickHandler);
       mapPinMain.removeEventListener('mousedown', pinMousedownHandler);
-    }
-  };
-
-  var pinClickHandler = function () {
-    activate();
-    mapPinMain.removeEventListener('mousedown', pinMousedownHandler);
-    mapPinMain.removeEventListener('click', pinClickHandler);
-  };
-  var crossClickHandler = function () {
-    var popup = map.querySelector('.popup');
-    var cross = map.querySelector('.popup__close');
-    popup.remove();
-    cross.removeEventListener('click', crossClickHandler);
-    document.removeEventListener('keydown', documentKeyDownHandler);
-  };
-  var documentKeyDownHandler = function (evt) {
-    if (evt.key === 'Escape') {
+      mapPinMain.removeEventListener('click', pinClickHandler);
+    };
+    var crossClickHandler = function () {
       var popup = map.querySelector('.popup');
+      var cross = map.querySelector('.popup__close');
       popup.remove();
+      cross.removeEventListener('click', crossClickHandler);
       document.removeEventListener('keydown', documentKeyDownHandler);
-    }
+    };
+    var documentKeyDownHandler = function (evt) {
+      if (evt.key === 'Escape') {
+        var popup = map.querySelector('.popup');
+        popup.remove();
+        document.removeEventListener('keydown', documentKeyDownHandler);
+      }
+    };
+    var pinsKeyDownHandler = function (evt) {
+      var target = evt.target;
+      if (evt.key === 'Enter') {
+        var index = target.children[0].dataset.indexNumber;
+        window.card.show(index);
+        var cross = map.querySelector('.popup__close');
+        cross.addEventListener('click', crossClickHandler);
+        document.addEventListener('keydown', documentKeyDownHandler);
+      }
+    };
+    var pinsClickHandler = function (evt) {
+      var target = evt.target;
+      if (target.tagName === 'IMG' && (target.parentElement.className.indexOf('map__pin--main') < 0)) {
+        var index = target.dataset.indexNumber;
+        window.card.show(index);
+        var cross = map.querySelector('.popup__close');
+        cross.addEventListener('click', crossClickHandler);
+        document.addEventListener('keydown', documentKeyDownHandler);
+      }
+    };
+    pins.addEventListener('keydown', pinsKeyDownHandler);
+    pins.addEventListener('click', pinsClickHandler);
+    mapPinMain.addEventListener('mousedown', pinMousedownHandler);
+    mapPinMain.addEventListener('click', pinClickHandler);
   };
-  var pinsKeyDownHandler = function (evt) {
-    var target = evt.target;
-    if (evt.key === 'Enter') {
-      var index = target.children[0].dataset.indexNumber;
-      window.card.show(index);
-      var cross = map.querySelector('.popup__close');
-      cross.addEventListener('click', crossClickHandler);
-      document.addEventListener('keydown', documentKeyDownHandler);
-    }
+  window.map = {
+    activateSite: activateSite
   };
-  var pinsClickHandler = function (evt) {
-    var target = evt.target;
-    if (target.tagName === 'IMG' && (target.parentElement.className.indexOf('map__pin--main') < 0)) {
-      var index = target.dataset.indexNumber;
-      window.card.show(index);
-      var cross = map.querySelector('.popup__close');
-      cross.addEventListener('click', crossClickHandler);
-      document.addEventListener('keydown', documentKeyDownHandler);
-    }
-  };
-  pins.addEventListener('keydown', pinsKeyDownHandler);
-  pins.addEventListener('click', pinsClickHandler);
-  mapPinMain.addEventListener('mousedown', pinMousedownHandler);
-  mapPinMain.addEventListener('click', pinClickHandler);
 })();
