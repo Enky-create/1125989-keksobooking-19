e723826c56a0e140da88cc1798da45f8ebc31a2b
Enky-create <komkowvadim@gmail.com>
2020-03-20 16:54:56 +0700

    Сбрасывает форму

--- js/init.js
+++ js/init.js
@@ -1,22 +1,29 @@
 'use strict';
 (function () {
-  var roomNumber = document.querySelector('#room_number');
-  var capacity = document.querySelector('#capacity');
-  var filters = document.querySelector('.map__filters');
-  var fieldsets = document.querySelectorAll('fieldset');
-  var addressInput = document.querySelector('#address');
-  var mapPinMain = document.querySelector('.map__pin--main');
-  var xString = mapPinMain.style.left.replace('px', '');
-  var yString = mapPinMain.style.top.replace('px', '');
-  var x = parseInt(xString, 10) + window.constant.MUFFIN_RADIUS;
-  var y = parseInt(yString, 10) + window.constant.MUFFIN_RADIUS;
-
-  for (var i = 0; i < fieldsets.length; i++) {
-    fieldsets[i].setAttribute('disabled', 'disabled');
-  }
-  addressInput.value = x + ', ' + y;
-  filters.classList.add('ad-form--disabled');
-  if (+roomNumber.value < +capacity.value) {
-    roomNumber.setCustomValidity('Количество комнат не соответствует количеству гостей');
-  }
+  var init = function () {
+    var form = document.querySelector('.ad-form');
+    var roomNumber = document.querySelector('#room_number');
+    var capacity = document.querySelector('#capacity');
+    var filters = document.querySelector('.map__filters');
+    var fieldsets = document.querySelectorAll('fieldset');
+    var addressInput = document.querySelector('#address');
+    var mapPinMain = document.querySelector('.map__pin--main');
+    var xString = mapPinMain.style.left.replace('px', '');
+    var yString = mapPinMain.style.top.replace('px', '');
+    var x = parseInt(xString, 10) + window.constant.MUFFIN_RADIUS;
+    var y = parseInt(yString, 10) + window.constant.MUFFIN_RADIUS;
+    for (var i = 0; i < fieldsets.length; i++) {
+      fieldsets[i].setAttribute('disabled', 'disabled');
+    }
+    addressInput.value = x + ', ' + y;
+    filters.classList.add('ad-form--disabled');
+    if (+roomNumber.value < +capacity.value) {
+      roomNumber.setCustomValidity('Количество комнат не соответствует количеству гостей');
+    }
+    form.reset();
+  };
+  window.init = {
+    doInit: init
+  };
+  window.init.doInit();
 })();
